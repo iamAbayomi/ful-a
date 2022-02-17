b95dbf91c271e3642784b2020a0fbf5a0138aa4e
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-02-17 18:28:20 +0100

    style: added size for the app image and logo

--- pages/verification/index.tsx
+++ pages/verification/index.tsx
@@ -5,13 +5,16 @@
      return (
          <div>
             <div className='main'>
-                <Image
-                    src={logo}
-                    alt="Picture of the author"
-                    width="50px"
-                    height="39px"
-                    layout="responsive" 
-                />
+              <div className='image-container'>
+                    <Image
+                        className='app-logo'
+                        src={logo}
+                        alt="Picture of the author"
+                        width="50"
+                        height="39"
+                        layout="responsive" 
+                    />
+                </div>
                 <p className="page-header">Verification</p>
                 <p className="page-subtitle">Enter verifcation code sent to you by the 
                     fulfillment center agent</p>
