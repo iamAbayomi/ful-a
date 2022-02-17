e2a8913e7f45efdfec4b05a708730d52c16b405b
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-02-17 18:13:53 +0100

    feat: added next/image

--- pages/verification/index.tsx
+++ pages/verification/index.tsx
@@ -1,10 +1,17 @@
- 
+ import Image from 'next/image'
+ import logo from './sendme-primary-logo.svg'
  
  const verification = () => {
      return (
          <div>
             <div className='main'>
-                <img src="/sendme-primary-logo.svg" />
+                <Image
+                    src={logo}
+                    alt="Picture of the author"
+                    width="50px"
+                    height="39px"
+                    layout="responsive" 
+                />
                 <p className="page-header">Verification</p>
                 <p className="page-subtitle">Enter verifcation code sent to you by the 
                     fulfillment center agent</p>
