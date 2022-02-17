a088b2172996dc86df41cc64bf47c64137399b2d
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-02-17 15:52:27 +0100

    feat: added storybook-ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
+import { NavItem } from '../storybook-ui-library/src/components/NavItem'
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
+    <h1>Same here</h1>
+      
     <p>
       <Link href="/about">
         <a>About</a>
