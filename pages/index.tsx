c69b2154f5103a26dbcedd69da06cd2e86db789d
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-02-17 17:13:15 +0100

    style: added global css file

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,15 @@ import { NavItem } from '../storybook-ui-library/src/components/NavItem'
 import { Button } from '../storybook-ui-library/src/components/SimpleButton'
 
 const IndexPage = () => (
-  <p>Home page</p>
+  <Layout title="Home | Next.js + TypeScript Example">
+    <h1>Same here</h1>
+      <Button children={'dadl'} variant={'primary'} />
+    <p>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+    </p>
+  </Layout>
 )
 
 export default IndexPage
--- pages/verification/index.tsx
+++ pages/verification/index.tsx
@@ -1,8 +1,17 @@
  
+ 
  const verification = () => {
      return (
          <div>
-            <p>Same here</p>
+            <div className='main'>
+                <img src="/sendme-primary-logo.svg" />
+                <p className="page-header">Verification</p>
+                <p className="page-subtitle">Enter verifcation code sent to you by the 
+                    fulfillment center agent</p>
+
+                <input className="input-field" type={"input"} name="Enter Code" />
+                <button className="red-button">Verify Code</button>
+            </div>
          </div>
      )
  }
