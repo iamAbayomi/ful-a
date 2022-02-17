fbde1dcbead63990eb9fc39360c8fbfe28bb5b7e
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-02-17 19:18:31 +0100

    style: added mulish font-family

--- pages/verification/index.tsx
+++ pages/verification/index.tsx
@@ -6,6 +6,10 @@
          <div>
             <div className='main'>
               <div className='image-container'>
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
+                    <link href={`https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,300;0,400;0,500;0,700;1,400;1,500&display=swap`} rel="stylesheet" />
+
                     <Image
                         className='app-logo'
                         src={logo}
