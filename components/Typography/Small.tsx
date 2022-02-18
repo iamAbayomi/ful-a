6b2b6eaa2c578017548bc3c6e43bc649335e9205
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-02-18 05:23:36 +0100

    style: added margin

--- components/Typography/Small.tsx
+++ components/Typography/Small.tsx
@@ -4,7 +4,7 @@ interface props{
     text: string
 }
 
-const Medium: React.FC<props> = (props) => (
+const Small: React.FC<props> = (props) => (
     <div>
         <Text className="page-subtitle">
             {props.text}
@@ -12,12 +12,12 @@ const Medium: React.FC<props> = (props) => (
     </div>
 )
 
-export default Medium
+export default Small
 
 
 const Text = styled.p`
-    font-size: 12px;
-    font-weight: 600;
-    margin: 0px 0px 0px;
-    color: #092443;
+    font-size: 10px;
+    margin: 20px 0px 5px;
+    color: #8F92A1;
+
 `
