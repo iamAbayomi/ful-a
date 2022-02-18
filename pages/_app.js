9700d80b977fa7a6f76bf40d1ac6003ee4bf5aff
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-02-18 04:48:25 +0100

    feat: added _app.js for css

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
 import '../css/App.css'
+import React from 'react'
+import Head from 'next/head'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <>
+        <Component {...pageProps} />
+      </>
+  )
 }
