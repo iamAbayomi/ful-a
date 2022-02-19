f4a6cb62c815e39b01aa0489dffa34c2143d61b6
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-02-19 05:49:21 +0100

    refactor: added authentication layout for signup

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,47 +1,28 @@
 import Image from 'next/image'
 import styled from 'styled-components'
 import logo from '../../assets/images/sendme-primary-logo.svg'
+import Layout from '../../components/Layouts/AuthenticationLayout'
 import Primary from '../../components/ui/InputField/Primary'
 import Medium from '../../components/ui/Typography/Medium'
 import Small from '../../components/ui/Typography/Small'
 
 const signup = () => (
-    <div>
-        <div className='main'>
-            <link rel="preconnect" href="https://fonts.googleapis.com"/>
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
-            <link href={`https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,300;0,400;0,500;0,700;0,800;1,400;1,500&display=swap`} rel="stylesheet"/>
-            <div className='image-container'>
-                <Image
-                    className='app-logo'
-                    src={logo}
-                    alt="Picture of the author"
-                    width="50"
-                    height="39"
-                    layout="responsive" 
-                />
-            </div>
-            <div className='sub-contents'>
-                <p className="page-header">Sign Up</p>
-                <p className="page-subtitle">
-                    Confirm your details and create password
-                    </p>
-                <Small text={'Full Name'} />
-                <Medium text={'Oluwabunmi Akinjide'}/>
-                <Small text={'Mobile Name'} />
-                <Medium text={'08104859684'}/>
-                <Small text={'Email Address'} />
-                <Medium text={'TimiD@gmail.com'}/>
-                
-                <div>
-                    <Primary placeholder={'Create new password'} type={'password'} />
-                    <Primary placeholder={'Confirm new password'} type={'password'} />
-                </div>
-
-                <button className="red-button">Reset Password</button>
-            </div>
+    <Layout>
+        <p className="page-subtitle">
+            Confirm your details and create password
+            </p>
+        <Small text={'Full Name'} />
+        <Medium text={'Oluwabunmi Akinjide'}/>
+        <Small text={'Mobile Name'} />
+        <Medium text={'08104859684'}/>
+        <Small text={'Email Address'} />
+        <Medium text={'TimiD@gmail.com'}/>
+        
+        <div>
+            <Primary placeholder={'Create new password'} type={'password'} />
+            <Primary placeholder={'Confirm new password'} type={'password'} />
         </div>
-    </div>
+    <Layout/>
 )
 
 export default signup
