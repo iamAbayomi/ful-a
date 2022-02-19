84522b0f3a65275362687c61238be06b94eb0284
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-02-19 06:06:29 +0100

    feat: added layout to signup

--- pages/verification/index.tsx
+++ pages/verification/index.tsx
@@ -1,16 +1,10 @@
  import Image from 'next/image'
-import { useRouter } from 'next/router'
+
  import Layout from '../../components/Layouts/AuthenticationLayout'
 
  const verification = () => {
-     const router = useRouter()
-     
-     function moveToSignUp(){
-         router.push('/signup')
-     }
-
      return (
-         <Layout pageTitle={'Verification'} buttonTitle={'Verify Code'} buttonOnClick={moveToSignUp}  >
+         <Layout pageTitle={'Verification'} buttonTitle={'Verify Code'} nextPage='/verification'  >
             <p className="page-subtitle">Enter verifcation code sent to you by the 
                 fulfillment center agent</p>
             <input className="input-field" type={"text"} placeholder="Enter Code" />
