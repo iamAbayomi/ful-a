0c3dcf5946dcd5a689f5d2ceb8b5d51afd558863
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-02-19 05:48:44 +0100

    refactor: changed component name

--- pages/verification/index.tsx
+++ pages/verification/index.tsx
@@ -1,6 +1,6 @@
  import Image from 'next/image'
 import { useRouter } from 'next/router'
- import Layout from '../../components/Layouts/VerificationLayout'
+ import Layout from '../../components/Layouts/AuthenticationLayout'
 
  const verification = () => {
      const router = useRouter()
@@ -10,7 +10,7 @@ import { useRouter } from 'next/router'
      }
 
      return (
-         <Layout buttonTitle={'Verify Code'} buttonOnClick={moveToSignUp} >
+         <Layout pageTitle={'Verification'} buttonTitle={'Verify Code'} buttonOnClick={moveToSignUp}  >
             <p className="page-subtitle">Enter verifcation code sent to you by the 
                 fulfillment center agent</p>
             <input className="input-field" type={"text"} placeholder="Enter Code" />
