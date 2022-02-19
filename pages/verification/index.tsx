a6a5996780992a352c25ead71abe4ab519b99647
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-02-19 06:18:08 +0100

    refactor: changed destination link to signup

--- pages/verification/index.tsx
+++ pages/verification/index.tsx
@@ -4,7 +4,7 @@
 
  const verification = () => {
      return (
-         <Layout pageTitle={'Verification'} buttonTitle={'Verify Code'} nextPage='/verification'  >
+         <Layout pageTitle={'Verification'} buttonTitle={'Verify Code'} nextPage='/signup'  >
             <p className="page-subtitle">Enter verifcation code sent to you by the 
                 fulfillment center agent</p>
             <input className="input-field" type={"text"} placeholder="Enter Code" />
