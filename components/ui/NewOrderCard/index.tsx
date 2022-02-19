e3a43946608cf07bd3301e66482bef239f3ff828
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-02-19 07:19:53 +0100

    style: changed the normal-text

--- components/ui/NewOrderCard/index.tsx
+++ components/ui/NewOrderCard/index.tsx
@@ -3,13 +3,13 @@ import styled from "styled-components"
 const NewOrderCard = () => (
     <div className="card">
         <div className="display-flex">
-            <p>Order</p>
+            <p className="newordertitle" >Order</p>
             <p>#1259</p>
         </div>
         <p className="highlight-text">Address </p>
-        <p>No 26, Alaakia Road, Bodija</p>
+        <p className="normal-text" >No 26, Alaakia Road, Bodija</p>
         <p className="highlight-text" >Delivery Time</p>
-        <p>3:00pm</p>
+        <p className="normal-text">3:00pm</p>
         
     </div>
 )
