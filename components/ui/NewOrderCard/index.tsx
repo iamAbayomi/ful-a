6487507b72bf113908886905035cb9772eb783af
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-02-19 07:34:54 +0100

    feat: added card image

--- components/ui/NewOrderCard/index.tsx
+++ components/ui/NewOrderCard/index.tsx
@@ -2,15 +2,19 @@ import styled from "styled-components"
 
 const NewOrderCard = () => (
     <div className="card">
-        <div className="display-flex">
-            <p className="newordertitle" >Order</p>
-            <p className="neworderno">#1259</p>
+        <div className="card-container">
+            <div className="display-flex">
+                <p className="newordertitle" >Order</p>
+                <div className="display-flex">
+                    <p className="neworderno">#1259</p>
+                </div>
+            </div>
+            <div className="neworder-line"/>
+            <p className="highlight-text">Address </p>
+            <p className="normal-text" >No 26, Alaakia Road, Bodija</p>
+            <p className="highlight-text" >Delivery Time</p>
+            <p className="normal-text">3:00pm</p>
         </div>
-        <p className="highlight-text">Address </p>
-        <p className="normal-text" >No 26, Alaakia Road, Bodija</p>
-        <p className="highlight-text" >Delivery Time</p>
-        <p className="normal-text">3:00pm</p>
-        
     </div>
 )
 
