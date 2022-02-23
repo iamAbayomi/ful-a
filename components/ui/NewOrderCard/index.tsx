ca535c097258d351b94238475505f0c6c2431d0e
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-02-24 00:08:12 +0100

    feat: added function to change the state of orders

--- components/ui/NewOrderCard/index.tsx
+++ components/ui/NewOrderCard/index.tsx
@@ -1,28 +1,36 @@
+import { useState } from "react"
 import styled from "styled-components"
 import rightArrow from '../../../assets/images/right-arrow.svg'
 
-const NewOrderCard = () => (
-    <div className="card ">
-        <div className="card-container">
-            <div className="display-flex">
-                <label className="container">
-                    <input type="checkbox" />
-                    <span className="checkmark"></span>
-                </label>
-                <p className="newordertitle" >Order</p>
+const NewOrderCard = () => {
+    const [checkboxState, setCheckboxState ] = useState<boolean>(false)
+    function changeCardBackgroundColor() {
+        let temp = checkboxState
+        setCheckboxState(!temp)
+    }
+    return(
+        <div className={`card ${checkboxState ? "card-highlight" : ''}` } >
+            <div className="card-container">
                 <div className="display-flex">
-                    <p className="neworderno">#1259</p>
-                    <RightArrowImage  src={rightArrow.src} />
+                    <label className="container">
+                        <input type="checkbox" onClick={changeCardBackgroundColor}/>
+                        <span className="checkmark"></span>
+                    </label>
+                    <p className="newordertitle" >Order</p>
+                    <div className="display-flex">
+                        <p className="neworderno">#1259</p>
+                        <RightArrowImage  src={rightArrow.src} />
+                    </div>
                 </div>
+                <div className="neworder-line"/>
+                <p className="highlight-text">Address </p>
+                <p className="normal-text" >No 26, Alaakia Road, Bodija</p>
+                <p className="highlight-text" >Delivery Time</p>
+                <p className="normal-text">3:00pm</p>
             </div>
-            <div className="neworder-line"/>
-            <p className="highlight-text">Address </p>
-            <p className="normal-text" >No 26, Alaakia Road, Bodija</p>
-            <p className="highlight-text" >Delivery Time</p>
-            <p className="normal-text">3:00pm</p>
         </div>
-    </div>
-)
+    )
+}
 
 
 export default NewOrderCard
