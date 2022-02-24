80654bc4da9e2685520a7fda1f4d49db0b5065aa
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-02-24 10:10:49 +0100

    refactor: created batched order component

---
+++ components/ui/BatchedOrderCard/index.tsx
@@ -0,0 +1,40 @@
+
+import optionsMenu from '../../../assets/images/options-menu.svg'
+import downArrow from "../../../assets/images/down-arrow.svg"
+import upArrow from "../../../assets/images/up-arrow.svg"
+import bicycle from "../../../assets/images/bicycle.svg"
+import bag from "../../../assets/images/bag.svg"
+
+const BatchedOrderCard = () => {
+    return (
+        <div className="card-elevated">
+                <div className="display-flex">
+                    <p>Batch 2</p>
+                    <p>#C267</p>
+                    <img src={optionsMenu.src} />
+                </div>
+                <div className="order-section display-flex-withoutspace">
+                    <img src={bag.src} />
+                    <p className="orders-notification">3 orders</p>
+                </div>
+                <div className={"display-flex"}>
+                    <div className="display-flex-withoutspace">
+                        <img src={bicycle.src} />
+                        <p className="orders-notification">Timi Busola</p>
+                    </div>
+                    <img src={downArrow.src} />
+                    {/* <img src={upArrow.src} /> */}
+                </div>
+                <div className="order-row display-flex">
+                    <div className="order-number">
+                        <p>Order 4</p>
+                        <p>#1270</p>
+                    </div>
+                    <p className="badge"> Package </p>
+                </div>
+        </div>
+    )
+}
+
+
+export default BatchedOrderCard
