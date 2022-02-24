0657e7c688ba8008470074cea1566d2ef1ff7fbd
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-02-24 17:05:16 +0100

    feat: added mulish fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,17 @@
 import '../assets/css/App.css'
+import '../assets/css/Backdrop.css'
 import '../assets/css/Dashboard.css'
 import '../assets/css/Checkbox.css'
 import '../assets/css/CreateNewBatch.css'
 import React from 'react'
 import Head from 'next/head'
+import MulishFonts from '../components/ui/MulishFonts'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
   return (
       <>
+        <MulishFonts />
         <Component {...pageProps} />
       </>
   )
