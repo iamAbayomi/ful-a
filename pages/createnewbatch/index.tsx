be456ded8a32322b93752027c3c55d963cb3bf87
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-02-24 16:08:47 +0100

    refactor: added new elements to create new batch

--- pages/createnewbatch/index.tsx
+++ pages/createnewbatch/index.tsx
@@ -1,25 +1,32 @@
 import styled from 'styled-components'
 import navArrow from '../../assets/images/nav-arrow.svg'
+import MulishFonts from '../../components/ui/MulishFonts'
 
 const CreateNewBatch =() => {
     return (
         <Background className="main dashboard">
-            <img src={navArrow.src} />
-            <p>Create New Batch</p>
-            <div className='batch-number-container'>
-                <p>Batch Number 4</p>
-                <input type={"name"} placeholder="#C397" />
-            </div>
-            <div className='assign-rider-container'>
-                <p>Assign Rider</p>
-                <input type={"name"} placeholder="Type rider's name or ID" />
-            </div>
+            <MulishFonts />
+            <div className='dashboard-contents'>
+                <img src={navArrow.src} />
+                <p className='page-title'>Create New Batch</p>
 
-            <div className='select-order-container'>
-                <p>Select Order</p>
-                <input type="" />
+                <div className='batch-number-container'>
+                    <p className='highlight-text'>Batch Number 4</p>
+                    <input type={"name"} className='input-text' placeholder="#C397" disabled />
+                </div>
+                
+                <div className='assign-rider-container'>
+                    <p className='highlight-text'>Assign Rider</p>
+                    <input type={"name"} className='input-text' placeholder="Type rider's name or ID" />
+                </div>
+
+                <div className='select-order-container'>
+                    <p className='highlight-text' >Select Order</p>
+                    <input type="name" className='input-text' />
+                </div>
+
+                <RedButton className='red-button'> Create Batch</RedButton>
             </div>
-            <button className='red-button'> Create Batch</button>
         </Background>
     )
 }
@@ -29,4 +36,17 @@ export default CreateNewBatch
 
 const Background = styled.div`
     background: white;
+`
+
+const RedButton = styled.div`
+    background: #FF1B03;
+    /* max-width: 300px; */
+    width: 100%;
+    height: 40px;
+    box-shadow: 0px 2px 10px rgb(255 145 139 / 50%);
+    border-radius: 8px;
+    border: 0px;
+    margin: 100px auto 0px auto;
+    color: white;
+    font-size: 14px;
 `
