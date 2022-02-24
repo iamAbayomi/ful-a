070d4647306aa427f6f954255b95237769b7cacb
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-02-24 10:27:45 +0100

    refactor: added background to create new batch

--- pages/createnewbatch/index.tsx
+++ pages/createnewbatch/index.tsx
@@ -1,9 +1,17 @@
+import styled from 'styled-components'
+import navArrow from '../../assets/images/nav-arrow.svg'
+
 const CreateNewBatch =() => {
     return (
-        <div>
-
-        </div>
+        <Background className="main dashboard">
+            <img src={navArrow.src} />
+        </Background>
     )
 }
 
 export default CreateNewBatch
+
+
+const Background = styled.div`
+    background: white;
+`
