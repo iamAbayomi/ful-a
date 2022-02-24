f1f5ac7ef3e9f625d00bfb8f852f7736f280c2c7
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-02-24 10:36:54 +0100

    refactor: aded input to assign readers

--- pages/createnewbatch/index.tsx
+++ pages/createnewbatch/index.tsx
@@ -5,6 +5,21 @@ const CreateNewBatch =() => {
     return (
         <Background className="main dashboard">
             <img src={navArrow.src} />
+            <p>Create New Batch</p>
+            <div className='batch-number-container'>
+                <p>Batch Number 4</p>
+                <input type={"name"} placeholder="#C397" />
+            </div>
+            <div className='assign-rider-container'>
+                <p>Assign Rider</p>
+                <input type={"name"} placeholder="Type rider's name or ID" />
+            </div>
+
+            <div className='select-order-container'>
+                <p>Select Order</p>
+                <input type="" />
+            </div>
+            <button className='red-button'> Create Batch</button>
         </Background>
     )
 }
