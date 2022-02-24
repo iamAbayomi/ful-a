ddad76aa4e7bd799f986826311d742f8edb546f8
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-02-24 16:33:01 +0100

    feat: added create batch components

--- pages/createnewbatch/index.tsx
+++ pages/createnewbatch/index.tsx
@@ -1,31 +1,39 @@
+import { useRouter } from 'next/router'
 import styled from 'styled-components'
 import navArrow from '../../assets/images/nav-arrow.svg'
 import MulishFonts from '../../components/ui/MulishFonts'
 
 const CreateNewBatch =() => {
+    const router = useRouter()
+
+    function moveToBatch() {
+        router.push('/batched')
+    }
+
     return (
         <Background className="main dashboard">
             <MulishFonts />
             <div className='dashboard-contents'>
-                <img src={navArrow.src} />
+                <img className='navigation-arrow' src={navArrow.src} onClick={moveToBatch} />
                 <p className='page-title'>Create New Batch</p>
+                <CreateNewBatchContainer className='createnewbatch-contents'>  
+                    <div className='batch-number-container'>
+                        <p className='highlight-text'>Batch Number 4</p>
+                        <input type={"name"} className='input-text' placeholder="#C397" disabled />
+                    </div>
+                    
+                    <div className='assign-rider-container'>
+                        <p className='highlight-text'>Assign Rider</p>
+                        <input type={"name"} className='input-text' placeholder="Type rider's name or ID" />
+                    </div>
 
-                <div className='batch-number-container'>
-                    <p className='highlight-text'>Batch Number 4</p>
-                    <input type={"name"} className='input-text' placeholder="#C397" disabled />
-                </div>
-                
-                <div className='assign-rider-container'>
-                    <p className='highlight-text'>Assign Rider</p>
-                    <input type={"name"} className='input-text' placeholder="Type rider's name or ID" />
-                </div>
+                    <div className='select-order-container'>
+                        <p className='highlight-text' >Select Order</p>
+                        <input type="name" className='input-text' />
+                    </div>
 
-                <div className='select-order-container'>
-                    <p className='highlight-text' >Select Order</p>
-                    <input type="name" className='input-text' />
-                </div>
-
-                <RedButton className='red-button'> Create Batch</RedButton>
+                    <RedButton className='red-button'> Create Batch</RedButton>
+                </CreateNewBatchContainer>
             </div>
         </Background>
     )
@@ -38,7 +46,7 @@ const Background = styled.div`
     background: white;
 `
 
-const RedButton = styled.div`
+const RedButton = styled.button`
     background: #FF1B03;
     /* max-width: 300px; */
     width: 100%;
@@ -46,7 +54,11 @@ const RedButton = styled.div`
     box-shadow: 0px 2px 10px rgb(255 145 139 / 50%);
     border-radius: 8px;
     border: 0px;
-    margin: 100px auto 0px auto;
+    margin: 100px auto 40px auto;
     color: white;
     font-size: 14px;
+`
+
+const CreateNewBatchContainer = styled.div`
+    margin: 30px 0px 0px;
 `
