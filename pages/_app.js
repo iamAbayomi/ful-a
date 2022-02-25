5b35906e20d7ee0e55aec763c3ebf80e4f008ea6
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-02-25 14:51:22 +0100

    feat: installed chakra

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,7 @@ import React from 'react'
 import Head from 'next/head'
 import MulishFonts from '../components/ui/MulishFonts'
 
+
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
   return (
