5822a7335acdba763b86d1d3ee8a775302753f5d
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-02-25 03:20:15 +0100

    style: designed verify package page

--- pages/verifypackage/index.tsx
+++ pages/verifypackage/index.tsx
@@ -1,14 +1,25 @@
+import styled from "styled-components"
 import OrderItemsComponent from "../../components/ui/OrderItemsComponent"
 
 const index = () => {
     return (
-        <div className="main">
-            <p>Verify Package</p>
-            <p>Input items in the package to confirm order items</p>
-            <input type="name" placeholder="Type item here" />
+        <Container className="main backdropdown">
+            <div className="backdropdown-line"></div>
+            <p className="backdropdown-text">Verify Package</p>
+            <p className="body-text">Input items in the package to confirm order items</p>
+            <input className="input-text" type="name" placeholder="Type item here" />
             <OrderItemsComponent />
-        </div>
+            <button className="red-button">Verify</button>
+            
+        </Container>
     )
 }
 
 export default index
+
+
+const Container = styled.div`
+    background-color: white ;
+    border: 1px solid rgb(0 0 0 / 12%);
+    height: 100vh;
+`
