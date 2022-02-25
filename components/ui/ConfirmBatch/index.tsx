435877f0a8d3cc1fe4cad490b78aa9561a4f1534
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-02-25 05:59:18 +0100

    feat: added bmodal to confirm order

---
+++ components/ui/ConfirmBatch/index.tsx
@@ -0,0 +1,40 @@
+import styled from "styled-components"
+import checkedBag from "../../../assets/images/checked-bag.svg"
+import closeButton from "../../../assets/images/close-button.svg"
+const ConfirmBatch =() => {
+    return(
+        <Container className="main">
+            <div>
+                <Close src={closeButton.src} />
+                <div className="center-div clear-float">
+                    <img src={checkedBag.src} />
+                </div>
+                <p className="heading-text">Confirm Batch 3 has been sent out</p>
+            </div>
+            <div className="center-div">
+                <RedButton className="red-button">Yes! I confirm</RedButton>
+            </div>
+        </Container>
+    )
+}
+
+export default ConfirmBatch
+
+const RedButton = styled.button`
+    color: white;
+    font-weight: 600;
+    font-size: 15px;
+`
+
+const Container = styled.div`
+    border: 1px solid #777e964f;
+    height: auto;
+    margin: 80px auto;
+    padding: 20px 10px 5px;
+    background: #FFFFFF;
+    border-radius: 20px;
+`
+
+const Close = styled.img`
+    float: right;
+`
--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -1,5 +1,6 @@
 import BackDrop from "../../components/ui/BackDrop"
 import BatchedOrderDetails from "../../components/ui/BatchedOrderDetails"
+import ConfirmBatch from "../../components/ui/ConfirmBatch"
 import Modal from "../../components/ui/Modal"
 import OrderDescription from "../../components/ui/OrderDescription"
 
@@ -9,7 +10,8 @@ const index = () => {
             {/* <BatchedOrderDetails /> */}
             {/* <BackDrop /> */}
             {/* <Modal /> */}
-            <OrderDescription />
+            {/* <OrderDescription /> */}
+            <ConfirmBatch />
         </div>
     )
 }
