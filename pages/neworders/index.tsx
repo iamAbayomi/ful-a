4633ce3e6888c9ef314dd104b4ecbe4751f959ea
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-03-02 16:58:04 +0100

    refactor: addded neworders icon

--- pages/neworders/index.tsx
+++ pages/neworders/index.tsx
@@ -1,9 +1,33 @@
 import styled from "styled-components";
-import Layout from "../../components/Layouts/NewOrders";
+import Layout from "../../components/Layouts/DashboardLayout";
+import KebabMenuDropdown from "../../components/ui/KebabMenuDropdown";
 import NewOrderCard from "../../components/ui/NewOrderCard";
+import redflag from '../../assets/images/red-flag.svg'
+import eventBus from "../../utils/eventBus";
+
+function selectOrders (item: any){
+    eventBus.dispatch("closeNewOrders", { message: "close Orders"})
+}
+
+
+const NewOrdersIcon = () => {
+    return(
+        <div className='display-flex header-icon'>
+            <div className='display-flex icon-container'>
+                <img
+                    className='red-flag-logo'
+                    src={redflag.src}
+                />
+            </div>
+            <div className='icon-container' style={ {margin: '0px 20px 0px 0px'}}>
+                <KebabMenuDropdown menuItems={['Select', 'Auto Batch Order']} menuItemMethod={selectOrders} />
+            </div>
+        </div>
+    )
+}
 
 const NewOrders = () => (
-    <Layout title="New Orders">
+    <Layout title="New Orders" headIcon={<NewOrdersIcon/>} >
         <NewOrderContainer >
        { [1,2,3,4,3].map((item) => (<NewOrderCard />))}
             
