90c98b6e0516d8f2f29a1264680e4e71d33cee79
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-03-02 13:24:21 +0100

    feat: added pages sentout

--- pages/sentout/index.tsx
+++ pages/sentout/index.tsx
@@ -1,13 +1,24 @@
+import styled from "styled-components"
 import Layout from "../../components/Layouts/DashboardLayout"
+import Search from "../../components/ui/Search/input"
 import SentOutOrdersCard from "../../components/ui/SentOutOrdersCard"
+
+
 const SentOut = () => {
     return(
-        <Layout title="Sent Out Orders">
-            {[1,2,3,4,5,6].map((item: any) => (
-                <SentOutOrdersCard />  
-            ))}
+        <Layout title="Sent Out Orders" headElements={<Search/>}>
+            <SentOutContainer>
+                {[1,2,3,4,5,6].map((item: any) => (
+                    <SentOutOrdersCard />  
+                ))}
+            </SentOutContainer>
         </Layout>
     )
 }
 
 export default SentOut
+
+const SentOutContainer = styled.div`
+    margin: 140px 0px 0px;
+    padding: 0px 0px 20px;
+`
