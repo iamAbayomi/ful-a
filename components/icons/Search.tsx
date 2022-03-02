249ec388fee65b42d7e376dbf9b6ef731b2c0a7c
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-03-02 13:24:37 +0100

    feat: added search icons

---
+++ components/icons/Search.tsx
@@ -0,0 +1,12 @@
+const Search = () => {
+    return(
+        <div>
+            <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="7.89447" cy="7.89545" r="7.11713" stroke="#8F92A1" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M12.8438 13.2153L15.6341 15.9984" stroke="#8F92A1" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+        </div>
+    )
+}
+
+export default Search
