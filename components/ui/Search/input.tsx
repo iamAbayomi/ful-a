8af5c7bd279b0beb28a701298320d3aeaee3f067
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-03-03 11:57:57 +0100

    refactor: added margin to the search container

--- components/ui/Search/input.tsx
+++ components/ui/Search/input.tsx
@@ -1,28 +1,35 @@
 import { Input, InputGroup, InputLeftElement, InputRightElement } from "@chakra-ui/react"
 import { Stack } from "@chakra-ui/layout"
 import SearchIcon from "../../icons/Search"
+import styled from "@emotion/styled"
 
 const Search = () => {
     return(
-        <Stack spacing={4}>
-                <InputGroup>
-                    <InputLeftElement
-                        pointerEvents = 'none'
-                        children= {<SearchIcon />}
-                    />
-                    <Input 
-                        borderWidth={'2px'} 
-                        borderColor={"#8f92a152"}
-                        focusBorderColor="gray.300" 
-                        placeholder="Search Order"  
-                    />
-                    <InputRightElement
-                        pointerEvents={"none"}
-                        children={<></>}
-                    />
-                </InputGroup>
-        </Stack>
+        <SearchContainer>
+            <Stack spacing={4}>
+                    <InputGroup>
+                        <InputLeftElement
+                            pointerEvents = 'none'
+                            children= {<SearchIcon />}
+                        />
+                        <Input 
+                            borderWidth={'2px'} 
+                            borderColor={"#8f92a152"}
+                            focusBorderColor="gray.300" 
+                            placeholder="Search Order"  
+                        />
+                        <InputRightElement
+                            pointerEvents={"none"}
+                            children={<></>}
+                        />
+                    </InputGroup>
+            </Stack>
+        </SearchContainer>
     )
 }
 
 export default Search
+
+const SearchContainer = styled.div`
+    margin: 10px 0px;
+`
