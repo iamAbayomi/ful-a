beeba6fcd3386251c66d7eb2c3189124b3c0ad80
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-03-04 13:45:44 +0100

    feat: created mock data for agent order

--- utils/sample-data.ts
+++ utils/sample-data.ts
@@ -16,3 +16,13 @@ export const sampleRiderData = [
   { ridersSectionTitle: 'New riders', ridersNo: 7, backgroundColor: "#DFDFDF"},
   { ridersSectionTitle: 'Suspended riders', ridersNo: 0, backgroundColor: "#FBCCC2"}
 ]
+
+
+export const agentOrderData = [
+  {agentOrdeNo:50, agentOrderTitle: "orders taken" , agentOderImage: "/bag-red.svg"},
+  {agentOrdeNo:4, agentOrderTitle: "batched orders" , agentOderImage: "/package-red.svg", backgroundColor: "#FBCCC2"},
+  {agentOrdeNo:10, agentOrderTitle: "packaged orders" , agentOderImage: "/shoppingbag-red.svg"},
+  {agentOrdeNo:9, agentOrderTitle: "sent out orders" , agentOderImage: "/bicycle-red.svg", backgroundColor: "#FBCCC2"},
+  {agentOrdeNo:20, agentOrderTitle: "completed orders" , agentOderImage: "/completed-orders-red.svg"},
+  {agentOrdeNo:3, agentOrderTitle: "red flags" , agentOderImage: "/redflag-red.svg", backgroundColor: "#FBCCC2"}
+]
