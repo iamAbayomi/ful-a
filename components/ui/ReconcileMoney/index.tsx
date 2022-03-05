bc2c53bf47e33cb00f6eb7425748cd6ebc2557d8
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-03-05 22:02:02 +0100

    feat: added reconcile component

---
+++ components/ui/ReconcileMoney/index.tsx
@@ -0,0 +1,26 @@
+const ReconcileCash  = () =>{
+    return(
+        <div>
+            <p>Reconcile Cash</p>
+            <p>Input Cash amount</p>
+            <input type={"name"} />
+            <button className="red-button">Confirm</button>
+        </div>
+    )
+}
+
+
+// const CashReconciledSuccesful
+
+const ReconcileMoney= () => {
+    return(
+        <div>
+            <p>Reconcile money</p>
+            <p>What do you want to reconcile first</p>
+            <button className="red-button">CAsh</button>
+            <button className="red-button">Continue</button>
+        </div>
+    )
+}
+
+export default ReconcileMoney
