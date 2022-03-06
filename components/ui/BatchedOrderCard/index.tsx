d189a75100842c4552c32afcdac5a08e642175ee
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-03-06 08:09:43 +0100

    refactor: added method to show dynamic row of orders

--- components/ui/BatchedOrderCard/index.tsx
+++ components/ui/BatchedOrderCard/index.tsx
@@ -1,5 +1,6 @@
 
 import optionsMenu from '../../../assets/images/options-menu.svg'
+import { Image } from '@chakra-ui/react'
 import downArrow from "../../../assets/images/down-arrow.svg"
 import upArrow from "../../../assets/images/up-arrow.svg"
 import bicycle from "../../../assets/images/bicycle.svg"
@@ -8,16 +9,31 @@ import styled from 'styled-components'
 import OrderRow from '../OrderRow'
 import { useRouter } from 'next/router'
 import KebabMenuDropdownVert from '../KebabMenuDropdownVert'
+import { useEffect, useRef, useState } from 'react'
+import Badge from '../Badge'
 
 type props = {
-    showOrderRow?: boolean
+    // showOrderRow?: boolean
 }
 
-const BatchedOrderCard = ({showOrderRow = false} : props) => {
+const downArrowSrc = '/down-arrow.svg'
+const upArrowSrc = '/up-arrow.svg'
+
+const BatchedOrderCard = () => {
     const router = useRouter()
+    const showOrdersRef = useRef(null)    
+    const [showOrderRow, setShowOderRow] = useState<boolean>(false)
+
     function moveToBatchedDetails(){
         router.push("/batchedorderdetails")
     }
+
+    function showOrders(){
+        let temp = !showOrderRow
+        setShowOderRow(temp)
+        // showOrdersRef.current.classList.toggle("hide")
+    }
+
     return (
         <div className="card-elevated">
             <BatchedOrderHeader className="display-flex">
@@ -25,7 +41,9 @@ const BatchedOrderCard = ({showOrderRow = false} : props) => {
                     <p className='batched-order-heading'>Batch 2</p>
                     <p className='batched-order-no'>#C267</p>
                 </div>
-                <KebabMenuDropdownVert menuItems={['Edit Batch', 'Confirm Sent Out']} menuItemMethod={() => {}} />
+                { router.pathname == "/sentout" ? <Badge badgeColor="#B7F1B687" badgeText={"Sent Out"} /> :
+                    <KebabMenuDropdownVert menuItems={['Edit Batch', 'Confirm Sent Out']} menuItemMethod={() => {}} />}
+                
             </BatchedOrderHeader>
             <div className="order-section display-flex-withoutspace">
                 <img src={bag.src} />
@@ -36,10 +54,11 @@ const BatchedOrderCard = ({showOrderRow = false} : props) => {
                     <img src={bicycle.src} />
                     <RidersName className="orders-notification">Timi Busola</RidersName>
                 </div>
-                <img src={downArrow.src} />
+                {/* <img  src={downArrow.src} /> */}
+                <Image onClick={showOrders} src={`${showOrderRow ?  upArrowSrc : downArrowSrc}`} />
                 {/* <img src={upArrow.src} /> */}
             </RidersNameContainer>
-            <OrderRowContainer className={`${showOrderRow ? '' : 'hide'}`} onClick={moveToBatchedDetails} >
+            <OrderRowContainer ref={showOrdersRef} className={`${showOrderRow ? '' : 'hide'}`} onClick={moveToBatchedDetails} >
                 <OrderRow />
                 <OrderRow/>
             </OrderRowContainer>
