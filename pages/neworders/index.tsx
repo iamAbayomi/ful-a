b2b9b2513a1e405846ff937acbced96c2505f8be
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-03-06 15:29:52 +0100

    feat: added checkbox to neworders component

--- pages/neworders/index.tsx
+++ pages/neworders/index.tsx
@@ -4,6 +4,7 @@ import KebabMenuDropdown from "../../components/ui/KebabMenuDropdown";
 import NewOrderCard from "../../components/ui/NewOrderCard";
 import redflag from '../../assets/images/red-flag.svg'
 import eventBus from "../../utils/eventBus";
+import CheckboxSelectedComponent from "../../components/ui/CheckboxSelectedComponent";
 
 function selectOrders (item: any){
     eventBus.dispatch("closeNewOrders", { message: "close Orders"})
@@ -12,22 +13,25 @@ function selectOrders (item: any){
 
 const NewOrdersIcon = () => {
     return(
-        <div className='display-flex header-icon'>
-            <div className='display-flex icon-container'>
-                <img
-                    className='red-flag-logo'
-                    src={redflag.src}
-                />
+        <div>
+            <div className='display-flex header-icon'>
+                <div className='display-flex icon-container'>
+                    <img
+                        className='red-flag-logo'
+                        src={redflag.src}
+                    />
+                </div>
+                <div className='icon-container' style={ {margin: '0px 20px 0px 0px'}}>
+                    <KebabMenuDropdown menuItems={['Select', 'Auto Batch Order']} menuItemMethod={selectOrders} />
+                </div>
             </div>
-            <div className='icon-container' style={ {margin: '0px 20px 0px 0px'}}>
-                <KebabMenuDropdown menuItems={['Select', 'Auto Batch Order']} menuItemMethod={selectOrders} />
-            </div>
+            {/* <CheckboxSelectedComponent /> */}
         </div>
     )
 }
 
 const NewOrders = () => (
-    <Layout title="New Orders" headIcon={<NewOrdersIcon/>} >
+    <Layout title="New Orders" headIcon={<NewOrdersIcon/>} headElements={<CheckboxSelectedComponent />} >
         <NewOrderContainer >
        { [1,2,3,4,3].map((item) => (<NewOrderCard />))}
             
