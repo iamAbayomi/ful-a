18f4f622771120803682f148ce1457feebab1596
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-03-09 10:36:51 +0100

    feat: added all modals to account reconcillation

--- components/ui/ReconcileMoney/index.tsx
+++ components/ui/ReconcileMoney/index.tsx
@@ -1,3 +1,6 @@
+import styled from "styled-components"
+import {Image} from "@chakra-ui/react"
+
 const ReconcileCash  = () =>{
     return(
         <div>
@@ -9,6 +12,48 @@ const ReconcileCash  = () =>{
     )
 }
 
+const CashReconciledSuccesful = () => {
+    return(
+        <div className="backdropdown">
+            {/* <div>
+                <Image 
+                    src="/check.svg"
+                    className="check-image"
+                    style={{ marginTop: "78px" }}
+                />
+                <Text>Cash reconciled successfully</Text>
+                <p className="order-subtitle">N20,000 total cash at hand. Cash account is balance</p>
+                <button className="red-button">Okay</button>
+            </div> */}
+            <CashReconciliationError />
+        </div>
+    )
+}
+
+const CashReconciliationError = () => {
+    return(
+        <div>
+            <Image 
+                src="/dangercircle.svg"
+                className="check-image"
+                style={{ marginTop: "78px" }}
+            />
+            <Text>Cash reconciliation error</Text>
+            <ErrorContainer className="">
+                <Image 
+                    src="/dangercircle.svg"
+                    className=""
+                    style={{  width: "40px" }}
+                />
+                <p className="order-subtitle">Cash account not balanced</p>
+            </ErrorContainer>
+            <button className="red-button">Reconcile cash account again</button>
+
+        </div>    
+    )
+}
+
+export default CashReconciledSuccesful
 
 // const CashReconciledSuccesful
 
@@ -16,14 +61,48 @@ const ReconcileMoney= () => {
     return(
         <div className="backdropdown">
             <div className="backdropdown-line"></div>
+            <Image 
+                src={"/close-button.svg"}
+                style={{float: "right"}}
+                // onClick={closeMethod}
+            />
             <p className="order-title">Reconcile money</p>
             <p className="order-subtitle">What do you want to reconcile first</p>
+            {/* <p className="order-title">Reconcile Cash</p>
+            <p className="order-subtitle">Input cash amount </p> */}
+
             <button className="button-red button-pink">Cash</button>
-            <button className="button-red">Continue</button>
+            {/* <InputText type={"name"} className="input-text" placeholder="N20,000" /> */}
+            <button className="red-button">Continue</button>
         </div>
     )
 }
 
-export default ReconcileMoney
+// export default ReconcileMoney
 
 //const s
+
+const InputText = styled.input`
+    margin: 30px 0px 40px;
+`
+
+const Text = styled.div`
+    font-family: Mulish;
+    font-style: normal;
+    font-size: 16px;
+    font-weight: 600;
+    /* line-height: 30px; */
+    clear: both;
+    margin: 10px auto 0px;
+    text-align: center;
+    color: #092443;
+    max-width: 200px;
+`
+
+
+const ErrorContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin: 72px auto 0px;
+    max-width: 180px;
+`
