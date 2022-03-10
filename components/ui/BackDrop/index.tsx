73b1c37d945789a9f3a8f9a22bfeb1323648784f
iamAbayomi <Oladinifolawiyo@gmail.com>
2022-03-10 09:09:26 +0100

    refactor: increased container height

--- components/ui/BackDrop/index.tsx
+++ components/ui/BackDrop/index.tsx
@@ -1,19 +1,25 @@
+
+import styled from "styled-components"
 import CheckboxSelectedComponent from "../CheckboxSelectedComponent"
 import NewOrderCard from "../NewOrderCard"
 
 const BackDrop = () =>{
     return (
         <div>
-            <div className="main backdropdown">
+            <Container className="main backdropdown">
                 <div className="backdropdown-line"></div>
                 <p className="backdropdown-text">Select Order</p>
                 <CheckboxSelectedComponent />
                 {[1,2,3,4,5].map(() => 
                     <NewOrderCard />
                 )}
-            </div>
+            </Container>
         </div>
     )
 }
 
 export default BackDrop
+
+const Container = styled.div`
+    height: auto;
+`
